1a29dc4e15698b17bf40b3bdc7f3c4ad517cc459
isaacgalvan10 <galvan1522@gmail.com>
2020-12-22 17:11:34 -0600

    adding contact me section and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './reset.css';
 import './app.css';
 import project1 from './imgs/Easybank-project.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 function App() {
   return (
@@ -12,7 +15,7 @@ function App() {
       <div id="about-section">
         <h2>About Me</h2>
         <p>From the ﬁrst "Hello World" I coded, I knew I was hooked into the amazing world of programming. Software development is more than a job to me, it has offered me a great way to continually learn and improve my skills. What started with a simple "Hello World" has become a full-ﬂedged passion that only gets more exciting as time goes by.  </p>
-        <button className="resume-btn">View Resume</button>
+        <a href="https://docs.google.com/document/d/1qql3RqjUkrPjJ-RWvTVawlUaWfb869-Kiryz2RZ6-u8/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><button className="resume-btn">View Resume</button></a>
       </div>
       <div id="project-section">
         <h2>My Projects</h2>
@@ -35,6 +38,16 @@ function App() {
           </div>
         </div>
       </div>
+        <div id="contact-section">
+          <h2>Contact Me</h2>
+          <p>galvan1522@gmail.com</p>
+          <p>(210)884-6564</p>
+        </div>
+        <footer>
+        <a className="github" href="https://github.com/isaacgalvan10" target="blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithubSquare} /></a>
+        <a className="linkedin" href="https://www.linkedin.com/in/isaac-galvan-712095149/" target="blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+          <p>Designed and Developed By Isaac Galvan</p>
+        </footer>
     </div>
   );
 }
