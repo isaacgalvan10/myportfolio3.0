ba7b585a4924fa83aba590305cd73670b687c2bd
isaacgalvan10 <galvan1522@gmail.com>
2020-12-29 12:52:57 -0600

    adding styles and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './reset.css';
 import './app.css';
 import project1 from './imgs/Easybank-project.png';
+import project2 from './imgs/react-saleswhale.png';
+import project3 from './imgs/social-dashboard-sm2.png';
+import project4 from './imgs/faq-accordion-card.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
@@ -20,28 +23,70 @@ function App() {
       <div id="project-section">
         <h2>My Projects</h2>
         <div className="project-left">
-          <img src={project1} alt="Easybank Project"/>
-          <p className="sub-title">Easybank Project</p>
-          <p>This is a static website that I remade from a front end challenge. I used React, SASS, and GSAP. Here is the design</p>
-          <div className="project-btns">
-          <button className="live-btn">View Live</button>
-          <button className="code-btn">View Code</button>
+          <div className="img-container">
+            <a href="https://easybank-ig.netlify.app/" target="blank">
+              <img src={project1} alt="Easybank Project"/>
+            </a>
+          </div>
+          <div className="content">
+            <p className="sub-title">Easybank Project</p>
+            <p>This is a static website that I remade from a front end challenge. I used React, SASS, and GSAP. Here is the design</p>
+            <div className="project-btns">
+            <a href="https://easybank-ig.netlify.app/" target="blank"><button className="live-btn">View Live</button></a>
+            <a href="https://github.com/isaacgalvan10/react-easybank" target="blank"><button className="code-btn">View Code</button></a>
+          </div>
+          </div>
+        </div>
+        <div className="project-right">
+          <div className="img-container">
+            <a href="https://react-saleswhale-isaacg.netlify.app/" target="blank">
+              <img src={project2} alt="SalesWhale Project"/>
+            </a>
+          </div>
+          <div className="content">
+            <p className="sub-title">Saleswhale Website</p>
+            <p>This is a static business website that I remade from a Figma Design. I used React, SASS, Bootstrap, and GSAP. Here is the figma design</p>
+            <div className="project-btns">
+            <a href="https://react-saleswhale-isaacg.netlify.app/" target="blank"><button className="live-btn">View Live</button></a>
+            <a href="https://github.com/isaacgalvan10/React-Saleswhale" target="blank"><button className="code-btn">View Code</button></a>
+          </div>
           </div>
         </div>
         <div className="project-left">
-          <img src={project1} alt="Easybank Project"/>
-          <p className="sub-title">Easybank Project</p>
-          <p>This is a static website that I remade from a front end challenge. I used React, SASS, and GSAP. Here is the design</p>
-          <div className="project-btns">
-          <button className="live-btn">View Live</button>
-          <button className="code-btn">View Code</button>
+          <div className="img-container">
+            <a href="https://ig-social-media-dashboard.netlify.app/" target="blank">
+              <img src={project3} alt="Easybank Project"/>
+            </a>
+          </div>
+          <div className="content">
+            <p className="sub-title">Social Media Dashboard Project</p>
+            <p>This is a frontend challenge from frontendmentor.io. I built this using Javascript, React, and SASS</p>
+            <div className="project-btns">
+            <a href="https://ig-social-media-dashboard.netlify.app/" target="blank"><button className="live-btn">View Live</button></a>
+            <a href="https://github.com/isaacgalvan10/social-media-dashboard" target="blank"><button className="code-btn">View Code</button></a>
+          </div>
+          </div>
+        </div>
+        <div className="project-right">
+          <div className="img-container">
+            <a href="https://isaacgalvan10.github.io/faq-accordion-card.io/" target="blank">
+              <img src={project4} alt="SalesWhale Project"/>
+            </a>
+          </div>
+          <div className="content">
+            <p className="sub-title">Saleswhale Website</p>
+            <p>This is a static business website that I remade from a Figma Design. I used React, SASS, Bootstrap, and GSAP. Here is the figma design</p>
+            <div className="project-btns">
+            <a href="https://isaacgalvan10.github.io/faq-accordion-card.io/" target="blank"><button className="live-btn">View Live</button></a>
+            <a href="https://github.com/isaacgalvan10/faq-accordion-card.io" target="blank"><button className="code-btn">View Code</button></a>
+          </div>
           </div>
         </div>
       </div>
         <div id="contact-section">
           <h2>Contact Me</h2>
-          <p>galvan1522@gmail.com</p>
-          <p>(210)884-6564</p>
+          <a href="mailto:galvan1522@gmail.com"><p className="email">galvan1522@gmail.com</p></a>
+          <a href="tel:+2108846564"><p>(210)884-6564</p></a>
         </div>
         <footer>
         <a className="github" href="https://github.com/isaacgalvan10" target="blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithubSquare} /></a>
